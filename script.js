4d69621ef2357e316538dad0b323e17f04386cd1
YugeshKK <95625392+YugeshKK@users.noreply.github.com>
2023-09-27 11:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const person = {
 const team=players;
 const team1=[...players];
 const cap1={
-	...person;
+	...person
 } 
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
